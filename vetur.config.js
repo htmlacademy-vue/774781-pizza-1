c3be78226570d7d3ac632a8497833be01a8b648e
evgn <stormdice23@gmail.com>
2021-11-09 00:40:33 +0500

    add vetur config

---
+++ vetur.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  projects: [
+    {
+      root: "./src/frontend",
+      package: "./package.json",
+      globalComponents: [
+        './src/**/*.vue'
+      ]
+    },
+  ],
+};

884c0866f834c4823e3252159b3b1d1d620f66cc
evgn <stormdice23@gmail.com>
2021-11-12 17:51:10 +0500

    add AppLayout.vue

---
+++ src/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <AppHeader />
+    <Index />
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/layouts/AppHeader.vue';
+import Index from '@/views/Index.vue';
+
+export default {
+  name: 'AppLayout',
+  components: {
+    AppHeader,
+    Index,
+  }
+}
+</script>

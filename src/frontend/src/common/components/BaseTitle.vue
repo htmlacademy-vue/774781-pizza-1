e566f30134384bd395eefabcf9c77eaa7f0cd917
evgn <stormdice23@gmail.com>
2021-11-21 18:15:32 +0500

    add styles for BaseTitle

--- src/frontend/src/common/components/BaseTitle.vue
+++ src/frontend/src/common/components/BaseTitle.vue
@@ -32,3 +32,21 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.title {
+  box-sizing: border-box;
+  width: 100%;
+  margin: 0;
+
+  color: $black;
+
+  &--big {
+    @include b-s36-h42;
+  }
+
+  &--small {
+    @include b-s18-h21;
+  }
+}
+</style>

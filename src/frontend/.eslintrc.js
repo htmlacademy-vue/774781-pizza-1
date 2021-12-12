eed4a6cf97d9fed6dd5d497a4342af486f5e722e
go-evgn <stormdice23@gmail.com>
2021-12-12 15:17:42 +0500

    update eslint for m1

--- src/frontend/.eslintrc.js
+++ src/frontend/.eslintrc.js
@@ -4,15 +4,12 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/base",
     "eslint:recommended",
+    "plugin:vue/base",
     "plugin:vue/essential",
     "plugin:vue/strongly-recommended",
     "plugin:vue/recommended",
   ],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",

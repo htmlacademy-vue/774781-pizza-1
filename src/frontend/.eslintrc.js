acaf7a7f6f5cf5dcddc24d4d8c24da83435ac545
go-evgn <stormdice23@gmail.com>
2021-12-12 21:21:32 +0500

    return initial config for eslint

--- src/frontend/.eslintrc.js
+++ src/frontend/.eslintrc.js
@@ -3,17 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/base",
-    "plugin:vue/essential",
-    "plugin:vue/strongly-recommended",
-    "plugin:vue/recommended",
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "comma-dangle": ["error", "always-multiline"],
   },
   overrides: [
     {

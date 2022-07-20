8ac4f6d7bdf701a17ecf04015a1d920ff5bfe8ca
evgenygorchakov <stormdice23@gmail.com>
2022-07-20 12:17:39 +0500

    add UserProfile component

---
+++ src/frontend/src/modules/profile/components/UserProfile.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="user">
+    <picture>
+      <source
+        type="image/webp"
+        srcset="img/users/user5@2x.webp 1x, img/users/user5@4x.webp 2x"
+      />
+      <img
+        src="img/users/user5@2x.jpg"
+        srcset="img/users/user5@4x.jpg"
+        alt="Василий Ложкин"
+        width="72"
+        height="72"
+      />
+    </picture>
+    <div class="user__name">
+      <span>Василий Ложкин</span>
+    </div>
+    <p class="user__phone">Контактный телефон: <span>+7 999-999-99-99</span></p>
+  </div>
+</template>
+
+<style lang="scss">
+.user {
+  display: flex;
+  align-items: center;
+
+  margin-bottom: 33px;
+}
+
+.user__name {
+  @include b-s20-h23;
+
+  margin-left: 30px;
+
+  span {
+    display: inline-block;
+
+    vertical-align: middle;
+  }
+}
+
+.user__button {
+  display: inline-block;
+
+  cursor: pointer;
+  vertical-align: middle;
+}
+</style>
--- src/frontend/src/views/ProfilePage.vue
+++ src/frontend/src/views/ProfilePage.vue
@@ -7,27 +7,7 @@
         <h1 class="title title--big">Мои данные</h1>
       </div>
 
-      <div class="user">
-        <picture>
-          <source
-            type="image/webp"
-            srcset="img/users/user5@2x.webp 1x, img/users/user5@4x.webp 2x"
-          />
-          <img
-            src="img/users/user5@2x.jpg"
-            srcset="img/users/user5@4x.jpg"
-            alt="Василий Ложкин"
-            width="72"
-            height="72"
-          />
-        </picture>
-        <div class="user__name">
-          <span>Василий Ложкин</span>
-        </div>
-        <p class="user__phone">
-          Контактный телефон: <span>+7 999-999-99-99</span>
-        </p>
-      </div>
+      <UserProfile />
 
       <div class="layout__address">
         <div class="sheet address-form">
@@ -130,11 +110,13 @@
 
 <script>
 import AppSidebar from "@/layouts/AppSidebar.vue";
+import { UserProfile } from "@/modules/profile/components";
 
 export default {
   name: "ProfilePage",
   components: {
     AppSidebar,
+    UserProfile,
   },
 };
 </script>

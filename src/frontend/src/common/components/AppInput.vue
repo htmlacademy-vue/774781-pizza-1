1f72b717643eeb072c09473d88b9ccbbece72779
evgenygorchakov <stormdice23@gmail.com>
2023-04-22 13:57:22 +0500

    Return style files

--- src/frontend/src/common/components/AppInput.vue
+++ src/frontend/src/common/components/AppInput.vue
@@ -25,7 +25,6 @@
 <script>
 export default {
   name: "AppInput",
-
   props: {
     type: {
       type: String,
@@ -60,7 +59,6 @@ export default {
       default: () => [],
     },
   },
-
   computed: {
     classModifier() {
       return {
@@ -71,60 +69,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.input {
-  display: block;
-
-  span {
-    @include r-s14-h16;
-
-    display: block;
-
-    margin-bottom: 4px;
-  }
-
-  input {
-    @include r-s16-h19;
-
-    display: block;
-
-    box-sizing: border-box;
-    width: 100%;
-    margin: 0;
-    padding: 8px 16px;
-
-    transition: 0.3s;
-
-    color: $black;
-    border: 1px solid $purple-400;
-    border-radius: 8px;
-    outline: none;
-    background-color: $white;
-
-    font-family: inherit;
-
-    &:focus {
-      border-color: $green-500;
-    }
-  }
-
-  &:hover {
-    input {
-      border-color: $black;
-    }
-  }
-
-  &--big-label {
-    display: flex;
-    align-items: center;
-
-    span {
-      @include b-s16-h19;
-
-      margin-right: 16px;
-
-      white-space: nowrap;
-    }
-  }
-}
-</style>
+<style lang="scss" src="../../assets/scss/blocks/input.scss" scoped></style>
